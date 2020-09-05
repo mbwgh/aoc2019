bdf7fa095baeead9ff0759cce1f1ca75ab344c76
Maximilian Winkler <mbw@mailbox.org>
2020-09-05 17:00:27 +0200

    Calculate angles for part 2

--- day10.py
+++ day10.py
@@ -127,10 +127,95 @@ Here are some larger examples:
 Find the best location for a new monitoring station. How many other asteroids
 can be detected from that location?
 
+--- Part Two ---
+
+Once you give them the coordinates, the Elves quickly deploy an Instant
+Monitoring Station to the location and discover the worst: there are simply too
+many asteroids.
+
+The only solution is complete vaporization by giant laser.
+
+Fortunately, in addition to an asteroid scanner, the new monitoring station
+also comes equipped with a giant rotating laser perfect for vaporizing
+asteroids. The laser starts by pointing up and always rotates clockwise,
+vaporizing any asteroid it hits.
+
+If multiple asteroids are exactly in line with the station, the laser only has
+enough power to vaporize one of them before continuing its rotation. In other
+words, the same asteroids that can be detected can be vaporized, but if
+vaporizing one asteroid makes another one detectable, the newly-detected
+asteroid won't be vaporized until the laser has returned to the same position
+by rotating a full 360 degrees.
+
+For example, consider the following map, where the asteroid with the new
+monitoring station (and laser) is marked X:
+
+.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##
+
+The first nine asteroids to get vaporized, in order, would be:
+
+.#....###24...#..
+##...##.13#67..9#
+##...#...5.8####.
+..#.....X...###..
+..#.#.....#....##
+
+Note that some asteroids (the ones behind the asteroids marked 1, 5, and 7)
+won't have a chance to be vaporized until the next full rotation. The laser
+continues rotating; the next nine to be vaporized are:
+
+.#....###.....#..
+##...##...#.....#
+##...#......1234.
+..#.....X...5##..
+..#.9.....8....76
+
+The next nine to be vaporized are then:
+
+.8....###.....#..
+56...9#...#.....#
+34...7...........
+..2.....X....##..
+..1..............
+
+Finally, the laser completes its first full rotation (1 through 3), a second
+rotation (4 through 8), and vaporizes the last asteroid (9) partway through its
+third rotation:
+
+......234.....6..
+......1...5.....7
+.................
+........X....89..
+.................
+
+In the large example above (the one with the best monitoring station location
+at 11,13):
+
+    The 1st asteroid to be vaporized is at 11,12.
+    The 2nd asteroid to be vaporized is at 12,1.
+    The 3rd asteroid to be vaporized is at 12,2.
+    The 10th asteroid to be vaporized is at 12,8.
+    The 20th asteroid to be vaporized is at 16,0.
+    The 50th asteroid to be vaporized is at 16,9.
+    The 100th asteroid to be vaporized is at 10,16.
+    The 199th asteroid to be vaporized is at 9,6.
+    The 200th asteroid to be vaporized is at 8,2.
+    The 201st asteroid to be vaporized is at 10,9.
+    The 299th and final asteroid to be vaporized is at 11,1.
+
+The Elves are placing bets on which will be the 200th asteroid to be vaporized.
+Win the bet by determining which asteroid that will be; what do you get if you
+multiply its X coordinate by 100 and then add its Y coordinate? (For example,
+8,2 becomes 802.)
+
 """
 
-from math import gcd
-from typing import List
+from math import atan2, gcd, pi, sqrt
+from typing import List, Tuple
 
 import numpy as np
 
@@ -228,6 +313,49 @@ def main1():
     matrix = parse_data(data)
     visibilities = calc_visibilities(matrix)
     print(np.max(visibilities))
+
+
+def angle(origin_x, origin_y, x_coord, y_coord):
+    """
+    Return the angle in radians between the vector that points upwards from
+    the given origin, and the vector between the origin and the vector with the
+    given coordinates.
+    See also: https://stackoverflow.com/questions/14066933/direct-way-of-computing-clockwise-angle-between-2-vectors/16544330#16544330
+    """
+
+    v_x, v_y = 0, 1
+    w_x, w_y = x_coord - origin_x, y_coord - origin_y
+
+    dot = v_x * w_x + v_y * w_y
+    det = v_x * w_y - v_y * w_x
+
+    return atan2(dot, det)
+
+
+def dist(x1, x2, y1, y2):
+    return sqrt((x1 - y1)**2 + (x2 - y2)**2)
+
+
+def all_angles(origin_x: int, origin_y: int, matrix: np.ndarray) -> List[
+        Tuple[int, int, float]]:
+    rows, cols = matrix.shape
+    angles = []
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i, j] and (i, j) != (origin_x, origin_y):
+                rad = angle(origin_x, origin_y, i, j)
+                if rad < 0:
+                    rad += 2 * pi
+                angles.append((i, j, rad))
+
+    angles.sort(
+        key=lambda tup: (tup[2], dist(origin_x, origin_y, tup[0], tup[1])))
+    return angles
+
+
+def max_index(matrix):
+    visibilities = calc_visibilities(matrix)
+    return np.unravel_index(np.argmax(visibilities), visibilities.shape)
 
 
 if __name__ == '__main__':
